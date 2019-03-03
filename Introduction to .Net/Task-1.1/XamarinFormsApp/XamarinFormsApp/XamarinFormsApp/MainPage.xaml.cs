22bf2ab9639d5bafe464aff978b2ff05e004b868
ivinnichek <vinnichekira@gmail.com>
2019-03-03 21:50:06 +0300

    Add Xamarin Forms app

---
+++ Introduction to .Net/Task-1.1/XamarinFormsApp/XamarinFormsApp/XamarinFormsApp/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinFormsApp
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private void HelloButton_Clicked(object sender, EventArgs e)
+		{
+			var name = NameEntry.Text;
+			Label.Text = $"Hello, {name}!";
+		}
+	}
+}
