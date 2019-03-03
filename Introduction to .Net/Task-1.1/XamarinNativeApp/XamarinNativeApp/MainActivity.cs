1120b3ef764051bfaafb8a73640161c32a4ad501
Vinnichek Iryna <vinnichekira@gmail.com>
2019-03-03 21:17:05 +0300

    Fix alignment

--- Introduction to .Net/Task-1.1/XamarinNativeApp/XamarinNativeApp/MainActivity.cs
+++ Introduction to .Net/Task-1.1/XamarinNativeApp/XamarinNativeApp/MainActivity.cs
@@ -15,17 +15,17 @@ namespace XamarinNativeApp
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
-
-			Button btn = FindViewById<Button>(Resource.Id.writeHelloButton);
-			btn.Click += this.Btn_Click;
+        
+	    Button btn = FindViewById<Button>(Resource.Id.writeHelloButton);
+            btn.Click += this.Btn_Click;
         }
 
-		private void Btn_Click(object sender, EventArgs e)
-		{
-			EditText editTextName = this.FindViewById<EditText>(Resource.Id.editTextName);
-			TextView textViewHello = this.FindViewById<TextView>(Resource.Id.textViewHello);
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            EditText editTextName = this.FindViewById<EditText>(Resource.Id.editTextName);
+            TextView textViewHello = this.FindViewById<TextView>(Resource.Id.textViewHello);
 
-			textViewHello.Text = $"Hello, {editTextName.Text}!";
-		}
-	}
+            textViewHello.Text = $"Hello, {editTextName.Text}!";
+        }
+    }
 }
