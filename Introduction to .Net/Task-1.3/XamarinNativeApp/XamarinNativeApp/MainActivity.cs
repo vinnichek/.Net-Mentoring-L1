1c8ee8a5f05b1e2a3d4ae16dddc419ea5f15f134
Vinnichek Iryna <vinnichekira@gmail.com>
2019-03-03 21:11:49 +0300

    Fix alignment

--- Introduction to .Net/Task-1.3/XamarinNativeApp/XamarinNativeApp/MainActivity.cs
+++ Introduction to .Net/Task-1.3/XamarinNativeApp/XamarinNativeApp/MainActivity.cs
@@ -8,7 +8,7 @@ using NetStandardClassLibrary;
 
 namespace XamarinNativeApp
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+[Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -17,16 +17,16 @@ namespace XamarinNativeApp
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
 
-			Button btn = FindViewById<Button>(Resource.Id.writeHelloButton);
-			btn.Click += this.Btn_Click;
+            Button btn = FindViewById<Button>(Resource.Id.writeHelloButton);
+            btn.Click += this.Btn_Click;
         }
 
-		private void Btn_Click(object sender, EventArgs e)
-		{
-			EditText editTextName = this.FindViewById<EditText>(Resource.Id.editTextName);
-			TextView textViewHello = this.FindViewById<TextView>(Resource.Id.textViewHello);
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            EditText editTextName = this.FindViewById<EditText>(Resource.Id.editTextName);
+            TextView textViewHello = this.FindViewById<TextView>(Resource.Id.textViewHello);
 
-			textViewHello.Text = ClassWriter.WriteHello(editTextName.Text);
-		}
-	}
+            textViewHello.Text = ClassWriter.WriteHello(editTextName.Text);
+        }
+    }
 }
